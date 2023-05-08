40094bf376af1d0b5a660fe29e5342155a5129ff
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-05-08 13:40:51 -0500

    integrated staged files into live repo

--- Develop/api/notes.js
+++ Develop/api/notes.js
@@ -13,23 +13,43 @@ notes.get("/api/notes", (req, res) => {
 
 // POST
 notes.post("/api/notes", (req, res) => {
+
+    // this takes the title and text properties from the request
+    // body  which is the data that is sent from the client to 
+    // server as a part of the HTTP request
     const { title, text } = req.body;
+
+    // this creates a new note object with the title,
+    // text and id properties. id is generated 
+    // using the uuidv4() function from the uuid package
     const newNote = {
         title: title,
         text: text,
         id: uuidv4(),
     }
 
+    // this reads the current contents of the ./db/db.json file 
+    // and stores it in the data variable.
     let data = fs.readFileSync("./db/db.json");
 
+    // this parses the data variable as JSON, converting it from a string to a 
+    // JavaScript object.
     data = JSON.parse(data)
 
+    // this adds newNote object to the data array
     data.push(newNote);
 
+    //this writes the updated data array to the ./db/db.json file,
+    // overwriting the previous contents.
     fs.writeFileSync('./db/db.json', JSON.stringify(data));
 
+    // this parses the data variable again to ensure that
+    // it is a JavaScript object, not a JSON  string.
     data = JSON.parse(data);
 
+    // this sends the data variable back to the client as a JSON response,
+    // indicating that the note was successfully created and providng
+    // the updated list of notes.
     res.json(data);
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const path = require('path');
-
-const { v4: uuidv4} = require('uuid');
-
-const apiRoutes = require('./routes/index');
-
+const apiRoutes = require('./api/notes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// using express to use middleware functions 
+// middleware functions 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use apiRoute
-app.use('/api', apiRoutes);
+// routes
+app.use(apiRoutes);
 
+// GET routes
+// for homepage
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
+
+// for note page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+// location of app
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
