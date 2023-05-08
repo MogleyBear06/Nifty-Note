39ddb9669a777e0e0dd385e0dcf2d40f559bcda0
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-05-08 12:57:21 -0500

    renamed and revised notes.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+const { v4: uuidv4} = require('uuid');
+
+const apiRoutes = require('./routes/index');
+
 const app = express();
-const apiRoutes = require('./routes/api.js');
-const htmlRoutes = require('./routes/html.js');
+const PORT = process.env.PORT || 3001;
 
+// using express to use middleware functions 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Use apiRoutes
+// Use apiRoute
 app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
