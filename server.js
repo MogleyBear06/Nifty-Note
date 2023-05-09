0ad4b26ab1bb28e048ebfa9c450c2904c20365a1
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-05-09 14:24:32 -0500

    what the fuuuuuuuuuck, fuck heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./api/notes');
+const apiRoutes = require('./Develop/api/notes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
